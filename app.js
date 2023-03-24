6bcbc4589e4429bf0f176f0fcbc62fee055307da
nicobucodiguito <nicocobucoco@gmail.com>
2023-03-24 16:42:59 -0300

    atucha-simulator alpha 0.1

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-endGame = false
-
-
 //EventEmitter for a pub/sub structure for events
 class EventEmitter {
     // a constructor is necessary to initialize the store for events and callbacks
@@ -70,6 +67,16 @@ class Atucha extends GameObject {
     }
 }
 
+class Edesur extends GameObject {
+    constructor(x, y) {
+      super(x, y);
+      (this.width = 200), (this.height = 200);
+      this.type = "Edesur"; 
+      this.speed = { x: 0, y: 0}
+    }
+}
+
+//creates the cat var so it can be used as an object
 let cat,
     eventEmitter = new EventEmitter();
 
@@ -80,17 +87,23 @@ const Messages = {
     KEY_EVENT_LEFT: "KEY_EVENT_LEFT",
     KEY_EVENT_RIGHT: "KEY_EVENT_RIGHT",
     KEY_EVENT_SPACE: "KEY_EVENT_SPACE",
-    COLLISION_ENEMY_LASER: "COLLISION_ENEMY_LASER",
-    COLLISION_ENEMY_HERO: "COLLISION_ENEMY_HERO",
+    GAME_END_ATUCHA: "GAME_END_ATUCHA",
+    ATUCHA_DOWN: "ATUCHA_DOWN",
+    KEY_EVENT_ENTER: "KEY_EVENT_ENTER",
+    EDESUR_DOWN: "EDESUR_DOWN",
+    GAME_END_EDESUR: "GAME_END_EDESUR",
 };
 
 //eventlistener that listens for key presses, especially for arrow keys 
 window.addEventListener("keydown", (evt) => {
     if (evt.key === "ArrowLeft") {
       eventEmitter.emit(Messages.KEY_EVENT_LEFT);
-
-    } else if (evt.key === "ArrowRight") {
+    } 
+    else if (evt.key === "ArrowRight") {
       eventEmitter.emit(Messages.KEY_EVENT_RIGHT);
+    }
+    else if(evt.key === "Enter") {
+      eventEmitter.emit(Messages.KEY_EVENT_ENTER);
     }
   });
 
@@ -112,6 +125,22 @@ function createAtucha() {
     gameObjects.push(atucha);
 }
 
+function createEdesur() {
+    edesur = new Edesur(
+      700,
+      704,
+    );
+    edesur.img = edesurImg;
+    gameObjects.push(edesur);
+}
+
+//returns a fully loaded sound with the path passed as a str parameter
+function createSound(path) {
+    sound = new Audio();
+    sound.src = path;
+    return sound
+};
+
 //function that takes care of loading assets with a path passed as a str parameter
 function loadAsset(path) {
     return new Promise((resolve) => {
@@ -123,6 +152,7 @@ function loadAsset(path) {
     });
 };
 
+
 function drawGameObjects(ctx) {
     gameObjects.forEach((go) => go.draw(ctx));
 }
@@ -132,28 +162,28 @@ function intersectRect(r1, r2) {
 }
 
 function updateGameObjects() {
+
   const atuchaBody = gameObjects.filter((go) => go.type === 'Atucha');
+
   atuchaBody.forEach((atucha) => {
     const catBody = cat.rectFromGameObject();
     if (intersectRect(catBody, atucha.rectFromGameObject())){
-      ctx.drawImage(boomImg, 0, 370);
-      function alertas() {
-        window.alert("nooo gatito que hiciste");
-        window.alert("exploto todo a la mierda y se quedo sin luz la mitad del pais");
-        window.alert(":(");
-      };
-      setTimeout(() =>
-      alertas()
-      , 300)
+      eventEmitter.emit(Messages.ATUCHA_DOWN);
+    }
+    else if (intersectRect(catBody, edesur.rectFromGameObject())){
+      apagonSound.play()
+      eventEmitter.emit(Messages.EDESUR_DOWN);
     }
   }
   )
+
 }
 
 function initGame() {
     gameObjects = [];
     createCat();
     createAtucha();
+    createEdesur();
 
 	eventEmitter.on(Messages.KEY_EVENT_LEFT, () => {
 		cat.x -= 10;
@@ -162,7 +192,50 @@ function initGame() {
 	eventEmitter.on(Messages.KEY_EVENT_RIGHT, () => {
 		cat.x += 10;
 	});
+
+  eventEmitter.on(Messages.ATUCHA_DOWN, () => {
+    eventEmitter.emit(Messages.GAME_END_ATUCHA)
+  });
+
+  eventEmitter.on(Messages.EDESUR_DOWN, () => {
+    displayMessage(
+      "fuiste a hacer un reclado a edesur, no te respondió nadie"
+    )
+  });
+
+  eventEmitter.on(Messages.GAME_END_ATUCHA, () => {
+    if (boomAtucha === false) { 
+      boomSound.play();
+      boomAtucha = true;
+    }
+    endGame(atucha);
+});
+
 }
+
+boomAtucha = false;
+
+function isCatDead() {
+  return cat.dead <= true;
+}
+
+function displayMessage(message, color = "black") {
+  ctx.font = "30px Arial";
+  ctx.fillStyle = color;
+  ctx.textAlign = "center";
+  ctx.fillText(message, canvas.width / 2, canvas.height / 2);
+}
+
+function endGame() {
+  if (atucha) {
+    displayMessage(
+      "explotó el atucha a la mierda"
+    );
+    apagonSound.pause();
+  }
+}
+
+cronica = false;
 
 //this defines the behaviour of everything once the page loads
 window.onload = async () => {
@@ -172,7 +245,13 @@ window.onload = async () => {
     atuchaImg = await loadAsset("assets/atucha.png");
     boomImg = await loadAsset("assets/boom.png");
     bgImg = await loadAsset("assets/bg.png");
-    
+    edesurImg = await loadAsset("assets/edesur.png");
+    boomSound = createSound("assets/boomsound.mp3");
+    apagonSound = createSound("assets/apagonSound.mp3");
+    cronicaImg = await loadAsset("assets/test.png");
+
+
+      
     //calling initgame function, basically creater the gameObjects array to later display objects,
     //and creates the objects themselves
     initGame();
@@ -181,6 +260,17 @@ window.onload = async () => {
       ctx.drawImage(bgImg, 0, 0)
       updateGameObjects();
       drawGameObjects(ctx);
+      if (boomAtucha === true) {
+        ctx.drawImage(boomImg, 0, 370);
+        setTimeout(function(){
+          cronica = true;
+        }, 3000)
+        
+      }
+      if (cronica === true) {
+          apagonSound.play();  
+          ctx.drawImage(cronicaImg, 0, 0);
+      }
 	}, 25);
 
 }
